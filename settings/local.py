bbbcbda6209ebe31e34a803bd35dfaa3e4b62e65
SrBrianRs <SrBrianRs@outlook.com>
2023-05-09 03:55:18 -0600

    xd

--- settings/local.py
+++ settings/local.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+
 ]
 
 ROOT_URLCONF = 'automoviles.urls'
@@ -128,4 +130,12 @@ STATIC_URL = '/static/'
 
 GRAPHENE = {
     'SCHEMA': 'automoviles.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
