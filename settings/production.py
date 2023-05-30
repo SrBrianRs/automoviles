e08e799f019f5d23c83bafc8d59f039fb5390b8d
SrBrianRs <SrBrianRs@outlook.com>
2023-05-30 13:33:39 -0600

    Update production.py

--- settings/production.py
+++ settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 's ubdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
